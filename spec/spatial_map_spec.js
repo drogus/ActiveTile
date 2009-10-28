f0432a29aa2c5cefe639a82e9a59752a91c5c1c3
Piotr Sarnacki <drogus@gmail.com>
2009-10-28 12:32:17 +0100

    Added some specs for SpatialMap (and found one bug while doing it :)

--- spec/spatial_map_spec.js
+++ spec/spatial_map_spec.js
@@ -11,6 +11,40 @@ Screw.Unit(function() {
         expect(map.cellSize).to(equal, 16);
       });
     });
+
+    describe("manipulating points", function() {
+      var map;
+
+      before(function() {
+        map = new SpatialMap();
+      });
+
+      it("should allow to store point in given cell", function() {
+        map.add({x: 10, y: 10});
+        var points = map.get(10, 10);
+        expect(points.length).to(equal, 1);
+        points = map.get(15, 15);
+        expect(points.length).to(equal, 1);
+        points = map.get(17, 17);
+        expect(points).to(equal, null);
+      });
+
+      it("should allow to get points from neighbourhood (given cell and all cells around)", function() {
+        map.add({x: 10, y: 10});
+        map.add({x: 20, y: 20});
+        map.add({x: 10, y: 20});
+        map.add({x: 50, y: 50});
+        var points = map.getAllFromNeighbourhood(20, 20);
+        expect(points.length).to(equal, 3);
+      });
+
+      it("should allow to clear points", function() {
+        map.add({x: 10, y: 10});
+        map.clear();
+        var points = map.get(10, 10);
+        expect(points).to(equal, null);
+      });
+    });
   });
 });
 
